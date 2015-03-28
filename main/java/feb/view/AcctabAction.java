c64da4e6a514bab4d14f158d528335819407fa88
wanglichao <wanglichao@163.com>
2015-03-28 16:21:40 +0800

    损益表excel导出jxls

--- main/java/feb/view/AcctabAction.java
+++ main/java/feb/view/AcctabAction.java
@@ -1,9 +1,12 @@
 package feb.view;
 
 import feb.service.DataExchangeService;
+import feb.service.JxlsManager;
 import gateway.sbs.core.domain.SOFForm;
 import gateway.sbs.txn.model.form.ac.T623;
 import gateway.sbs.txn.model.msg.M8621;
+import javafx.scene.input.DataFormat;
+import net.sf.jxls.transformer.XLSTransformer;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.slf4j.Logger;
@@ -13,9 +16,10 @@ import pub.tools.MessageUtil;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * Created by Lichao.W At 2015/3/24 9:17
@@ -28,10 +32,12 @@ public class AcctabAction implements Serializable {
 
     @ManagedProperty(value = "#{dataExchangeService}")
     private DataExchangeService dataExchangeService;
+    private String curcde = "001";
     M8621 m8621 = new M8621();
     List<T623.Bean> dataList = new ArrayList<>();
 
     public void onAllQry(){
+        m8621.setCURCDE(curcde);
         List<SOFForm> formList = dataExchangeService.callSbsTxn("8621", m8621);
         if (formList != null && !formList.isEmpty()) {
             dataList = new ArrayList<>();
@@ -48,22 +54,46 @@ public class AcctabAction implements Serializable {
             }
         }
     }
+    public String onExpExcel() {
+        try {
+            if (dataList == null || dataList.size() == 0) {
+                MessageUtil.addWarn("未查询数据！");
+                return null;
+            }
+            BigDecimal ctot6 = null; //支出合计
+            BigDecimal dtot5 = null;  //收入合计:
+            BigDecimal jtot6 = null;  //收入合计:
+            BigDecimal stot5 = null;  //结损:::
+            List<T623.Bean> dataList6 = new ArrayList<>();
+            List<T623.Bean> dataList5 = new ArrayList<>();
+            for (T623.Bean datas:dataList){
+                if ("2".equals(datas.getPLTYPE())){
+                    if ("3".equals(datas.getPLCLAS())){
+                        ctot6 = datas.getPLAMNT();
+                    }else if ("4".equals(datas.getPLCLAS())){
+                        jtot6 = datas.getPLAMNT();
+                    }else {
+                        dataList6.add(datas);
+                    }
+                }else if ("1".equals(datas.getPLTYPE())){
+                    dataList5.add(datas);
+                }
+            }
+            Date expdat = new Date();
+            String excelFilename = "损益表" + ".xls";
+            JxlsManager jxls = new JxlsManager();
+            Map beansMap = new HashMap();
+            beansMap.put("record6s", dataList6);
+            beansMap.put("record5s", dataList5);
+            beansMap.put("curcde", curcde);
+            beansMap.put("expdat", expdat);
+            beansMap.put("ctot6", ctot6);
+            beansMap.put("jtot6", jtot6);
+            jxls.exportDataToXls(excelFilename, "/tmp.xls", beansMap);
+        } catch (Exception e) {
 
-    public void xlsExport(Object document){
-        HSSFWorkbook wb = (HSSFWorkbook) document;
-        HSSFSheet sheet = wb.getSheetAt(0);
-        HSSFRow header = sheet.getRow(0);
-
-        HSSFCellStyle cellStyle = wb.createCellStyle();
-        cellStyle.setFillForegroundColor(HSSFColor.GREEN.index);
-        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
-
-        for(int i=0; i < header.getPhysicalNumberOfCells();i++) {
-            HSSFCell cell = header.getCell(i);
-
-            cell.setCellStyle(cellStyle);
         }
-
+        return null;
     }
 
     //= = = = = = = = = = = get set = = = = = = = =
@@ -74,6 +104,14 @@ public class AcctabAction implements Serializable {
 
     public void setDataExchangeService(DataExchangeService dataExchangeService) {
         this.dataExchangeService = dataExchangeService;
+    }
+
+    public String getCurcde() {
+        return curcde;
+    }
+
+    public void setCurcde(String curcde) {
+        this.curcde = curcde;
     }
 
     public M8621 getM8621() {
